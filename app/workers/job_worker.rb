3478f821c3edda192fc0721d1eb6edb53eff925f
Edivan de Castro <edivandecastro@gmail.com>
2021-12-10 00:04:35 -0300

    Saved event in database with JobWorker

--- app/workers/job_worker.rb
+++ app/workers/job_worker.rb
@@ -2,6 +2,16 @@ class JobWorker
   include Sidekiq::Worker
 
   def perform(*args)
-    puts "teste"
+    id, event_attributes = args
+
+    Event.where(active: false).each do |event|
+      if event.id.to_s == id
+        event.update(event_attributes)
+      else
+        event.active = true
+        sleep 1
+        event.save
+      end
+    end
   end
 end
