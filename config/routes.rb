5e3264acd1346059145abe098725b38c21dc71ae
Edivan de Castro <edivandecastro@gmail.com>
2021-12-10 00:03:06 -0300

    Added route for dashbord sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/jobs/push/:id', to: 'jobs#push'
   get '/jobs/perform/:id', to: 'jobs#perform'
+
+  mount Sidekiq::Web => '/sidekiq'
 end
